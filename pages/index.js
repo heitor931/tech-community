d6e996450292c6eb7c3998d9857f8c6ffad99a3a
Engineer <heitorino1@gmail.com>
2023-01-10 17:34:26 +0100

    Adicionado title and header

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
+import Head from "next/head";
+
 import HomePage from "../Components/HomePage";
 function Page() {
   return (
-    <div className="mainPage">
-      <h1 className="title">Comunidade Telco Development & Ideas</h1>
-      <HomePage />
-    </div>
+    <>
+      <Head>
+        <title>Telco community & Ideas</title>
+        <meta
+          property="og:title"
+          content="Telco community & ideas"
+          key="title"
+        />
+      </Head>
+
+      <div className="mainPage">
+        <h1 className="title">Comunidade Telco Development & Ideas</h1>
+        <HomePage />
+      </div>
+    </>
   );
 }
 
